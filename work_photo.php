ab1df1f9eb7e10b81891518f4b58742250eac522
山田智彦 <tmhik.ymd@gmail.com>
2017-04-04 14:04:51 +0900

    新規

---
+++ work_photo.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, user-scalable=yes, maximum-scale=1.0, minimum-scale=1.0">
+<title>四恩学園 採用サイトです</title>
+<meta name="keywords" content="">
+<meta name="description" content="">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+<link rel="stylesheet" type="text/css" href="css/work_style.css" media="screen">
+<script src="js/jquery1.7.2.min.js"></script>
+<script src="js/script.js"></script>
+</head>
+
+<body>
+
+<?php include (dirname(__FILE__).'/parts/header.html'); ?>
+
+<?php include (dirname(__FILE__).'/parts/gnav.html'); ?>
+
+<div id="mainImg">
+	<img src="images/mainImage0201.png" width="940" height="380" alt="">
+</div>
+
+<div id="wrapper">
+
+	<div id="content">
+		<section>
+			<article class="content">
+				<div class="post">
+					<h1><span>フォトギャラリー</span></h1>
+<div id="contents">
+
+    <p>
+    写真一覧です。
+    </p>
+
+    <div class="pic"></div>
+    <div class="pic"></div>
+    <div class="pic"></div>
+    <div class="pic"></div>
+    <div class="pic"></div>
+    <div class="pic"></div>
+    
+</div>
+			</article>
+		</section>
+	</div><!-- / content -->
+
+</div>
+<!-- / wrapper -->
+
+<?php include (dirname(__FILE__).'/parts/footer.html'); ?>
+<!-- / footer -->
+
+</body>
+</html>
